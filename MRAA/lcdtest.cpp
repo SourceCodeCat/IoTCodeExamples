e6c01c609ae5492b840cc8204ccbd82191c33cf1
Marco Antonio Salgado Martinez <ing.marco@gmail.com>
2016-04-24 22:58:10 -0500

    Update lcdtest.cpp

--- MRAA/lcdtest.cpp
+++ MRAA/lcdtest.cpp
@@ -6,16 +6,15 @@ int
 main(int argc, char **argv)
 {
  
-    // 0x62 RGB_ADDRESS, 0x3E LCD_ADDRESS
-        upm::Jhd1313m1 *lcd = new upm::Jhd1313m1(0, 0x3E, 0x62);
-            lcd->setCursor(0,0);
-	     lcd->setColor(0, 255, 0);
-                lcd->write("Hello World");
-                    lcd->setCursor(1,2);
-                        lcd->write("Hello World");
-
-                            printf("Sleeping for 5 seconds\n");
-                               sleep(5);
-                                  delete lcd;
-                                        return 0;     
+	// 0x62 RGB_ADDRESS, 0x3E LCD_ADDRESS
+	upm::Jhd1313m1 *lcd = new upm::Jhd1313m1(0, 0x3E, 0x62);
+	lcd->setCursor(0,0);
+	lcd->setColor(0, 255, 0);
+	lcd->write("Hello World");
+	lcd->setCursor(1,2);
+        lcd->write("Hello World");
+	printf("Sleeping for 5 seconds\n");
+        sleep(5);
+        delete lcd;
+        return 0;     
 }	
